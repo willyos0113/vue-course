ca7c6e71d8c144ab4017fbca901f6836ed530959
yiwei <willyos0113@gmail.com>
2025-05-10 20:42:16 +0800

    vue udemy course section 3 - props

---
+++ 04-vue-core/6/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import MessageItem from "./components/MessageItem.vue";
+export default {
+  components: { MessageItem },
+};
+</script>
+
+<template>
+  <main>
+    <div>
+      <MessageItem sender="張三" msg="你好呀!" time="19:38" />
+      <MessageItem sender="李四" msg="Hi!" time="19:40" />
+      <MessageItem sender="王五" msg="最近怎麼樣？" time="19:42" />
+    </div>
+  </main>
+</template>
+
+<style>
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+</style>
