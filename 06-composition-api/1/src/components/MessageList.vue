3e1f6f68e72e6fb88b1c0f7b07e0fcce2f8b171a
yiwei <willyos0113@gmail.com>
2025-05-22 01:00:15 +0800

    vue udemy course section 6 - 組合 API 的 computed() 結束

--- 06-composition-api/1/src/components/MessageList.vue
+++ 06-composition-api/1/src/components/MessageList.vue
@@ -1,30 +1,34 @@
 <template>
-  <h2>{{ options.title }}</h2>
-  <p>
-    用戶: {{ options.user.name }}, 活躍:
-    {{ options.user.active ? "是" : "否" }}
-  </p>
-  <ul>
-    <!-- 遍歷 messages 重複渲染 -->
-    <li v-for="msg in messages" :key="msg.id">{{ msg.content }}</li>
-  </ul>
-  <button @click="messages = []">刪除全部</button>
-  <button @click="options.title = '這是標題'">修改標題</button>
-  <button @click="options.user.name = '李四'">修改用戶名</button>
+  <div>
+    <input type="text" placeholder="搜索訊息" v-model="searchTerm" />
+    <h2>{{ options.title }}</h2>
+    <p>
+      用戶: {{ options.user.name }}, 活躍:
+      {{ options.user.active ? "是" : "否" }}
+    </p>
+    <ul>
+      <li v-for="msg in searchedMessages" :key="msg.id">{{ msg.content }}</li>
+    </ul>
+    <button @click="messages = []">刪除全部</button>
+    <button @click="options.title = '這是標題'">修改標題</button>
+    <button @click="options.user.name = '李四'">修改用戶名</button>
+  </div>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { ref, reactive, computed } from "vue";
 
 export default {
   setup() {
-    // ref() 將任何形態數據轉換成響應性數據
+    // 1.1. ref() 將任何形態數據轉換成響應性數據
     const messages = ref([
       { id: 1, content: "這是一條訊息提醒1" },
       { id: 2, content: "這是一條訊息提醒2" },
       { id: 3, content: "這是一條訊息提醒3" },
       { id: 4, content: "這是一條訊息提醒4" },
     ]);
+
+    // 1.2. reactive() 只能將物件轉換成響應性數據
     const options = reactive({
       title: "消息列表",
       user: {
@@ -32,12 +36,24 @@ export default {
         active: true,
       },
     });
-    // ref() 包裝的響應性數據 .value 才能存取他的值
+
+    const searchTerm = ref("");
+    // 1.3. 定義 computed() 函數回傳計算結果
+    const searchedMessages = computed(() => {
+      if (searchTerm.value === "") return messages.value;
+      return messages.value.filter((msg) => {
+        return msg.content.includes(searchTerm.value); // 包含 searchTerm 的部分才顯示
+      });
+    });
+
+    // 1.1. ref() 包裝的響應性數據 .value 才能存取他的值
     console.log(messages.value);
-    // reactive() 包裝的響應性數據，可以直接存取他的值
+
+    // 1.2. reactive() 包裝的響應性數據，可以直接存取他的值
     console.log(messages);
-    // 必須回傳物件屬性，才能在模板上存取
-    return { messages, options };
+
+    // ※ 必須回傳物件屬性，才能在模板上存取
+    return { messages, options, searchTerm, searchedMessages };
   },
 };
 </script>
